6e5a7c51a2154c413481466ac4c00f786cfdfb71
HawraHusain <hawraanowrooz158@gmail.com>
2025-02-04 20:51:40 +0300

    Solved

---
+++ src/components/PetList/PetList.jsx
@@ -0,0 +1,30 @@
+
+const PetList = (props) => {
+return (
+    <div>
+      <h1>Pet List</h1>
+      <div>
+        {!props.pets.length ? (
+          <h2>No Pets Yet!</h2>
+        ) : (
+          <ul>
+            {props.pets.map((pet) => 
+              <li 
+                key={pet._id}
+                onClick={() => props.handleSelect(pet)}
+              >
+                {pet.name}
+              </li>
+            )}
+          </ul>
+        )}
+      </div>
+      <button onClick={props.handleFormView}>
+        {props.isFormOpen ? 'Close Form' : 'New Pet'}
+      </button>
+    </div>
+  );
+
+};
+
+export default PetList;
